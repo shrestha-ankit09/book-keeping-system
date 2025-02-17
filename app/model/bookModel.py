bb233ecd0bd8fa8aa55b5d65fac8334aab99c70a
Ankit Shrestha <ankit@krispcallmail.com>
2025-02-17 10:19:26 +0545

    Add user and book model and also the schema

---
+++ app/model/bookModel.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from app.database.db import Base
+from datetime import datetime, timezone
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+    isbn = Column(String, unique=True, nullable=False)
+
+    title = Column(String, nullable=False)
+
+    author = Column(String, nullable=False)
+
+    createdAt = Column(DateTime, default=datetime.now(timezone.utc))
+    updatedAt = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("User", back_populates="books")      
