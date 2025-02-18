73c3fad3a08d212d71543ff3f00c8571d264962f
Ankit Shrestha <ankit@krispcallmail.com>
2025-02-18 09:11:11 +0545

    Add get book via isbn

--- app/schema/query.py
+++ app/schema/query.py
@@ -1,5 +1,6 @@
 from ariadne import QueryType
 from app.model.userModel import User
+from app.model.bookModel import Book
 from app.database.db import SessionLocal
 from sqlalchemy.orm import Session
 from graphql.error import GraphQLError 
@@ -28,10 +29,41 @@ def resolve_get_user(_, info, email: str):
 
 @query.field("getBooks")
 def resolve_get_books(_, info):
-    pass
+    session: Session = SessionLocal()
+
+    try:
+        books = session.query(Book).all()
+
+        if not books:
+            return []
+        
+        books_data = [
+            {key:value for key, value in book.__dict__.items() if not key.startswith('_')} 
+            for book in books
+        ]
+
+        return books_data
+    except Exception as e:
+        raise GraphQLError(f"Error fetching book data: {str(e)}")
+    finally:
+        session.close()
 
 
 @query.field("getBook")
 def resolve_get_book(_, info, isbn: str):
-    print(isbn)
-    pass
+    session: Session = SessionLocal()
+
+    try:
+        book = session.query(Book).filter(Book.isbn == isbn).first()
+
+        if not book:
+            return {"message": "Book not found with that isbn number"}
+        
+        book_data = {key:value for key, value in book.__dict__.items() if not key.startswith('_')}
+
+        return book_data
+
+    except Exception as e:
+        raise GraphQLError(f"Error Fetching data of isbn number {isbn}, error: {str(e)}")
+    finally:
+        session.close()
